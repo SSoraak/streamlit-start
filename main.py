6440996cf7a57cbd6366fb4b46b7739c78875cb1
Sorasak Sai-in <91424283+SSoraak@users.noreply.github.com>
2025-04-09 16:40:35 +0700

    0

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ if page == "📊 Dashboard":
     st.header("📊 Dashboard Overview")
     dashboard_url = "https://your-dashboard-url.com"  # <== ใส่ URL dashboard จริงตรงนี้
     st.markdown(f"""
-        <iframe title="ข้อมูลเครื่องจักร MA (ธีม)" width="100%" height="100%" src="https://app.powerbi.com/reportEmbed?reportId=892b0a03-c03d-41d9-a3fd-79ac05cbca3d&autoAuth=true&ctid=1f23f2e8-f8b5-4438-a7d7-1d2d87dffcf3" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="ข้อมูลเครื่องจักร MA (ธีม)" width="100%" height="750" src="https://app.powerbi.com/reportEmbed?reportId=892b0a03-c03d-41d9-a3fd-79ac05cbca3d&autoAuth=true&ctid=1f23f2e8-f8b5-4438-a7d7-1d2d87dffcf3" frameborder="0" allowFullScreen="true"></iframe>
     """, unsafe_allow_html=True)
 
 elif page == "🔧 Predict Maintenance":
